3aaefd9132b78a766e20deddf0236c665c6be9f4
Fabian Mäkilä <contact@fabianadrian.fi>
2026-02-26 01:37:09 +0200

    add new migration

---
+++ common/src/main/resources/db/migration/mariadb/V2__foreign_key_cascade.sql
@@ -0,0 +1,8 @@
+ALTER TABLE dw_minecraft_profiles
+    DROP FOREIGN KEY dw_minecraft_profiles_ibfk_1;
+
+ALTER TABLE dw_minecraft_profiles
+    ADD CONSTRAINT fk_dw_minecraft_profiles_dw_discord_profiles_discord_identifier
+        FOREIGN KEY (discord_identifier)
+            REFERENCES dw_discord_profiles(discord_identifier)
+            ON DELETE CASCADE;
