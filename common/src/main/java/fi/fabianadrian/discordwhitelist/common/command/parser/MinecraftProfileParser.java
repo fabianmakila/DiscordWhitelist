cc9e8ad7e04334fde9e49a47b2ea6fca86ee2adb
Fabian Mäkilä <contact@fabianadrian.fi>
2026-02-26 14:37:35 +0200

    MinecraftProfileParser refactor

--- common/src/main/java/fi/fabianadrian/discordwhitelist/common/command/parser/MinecraftProfileParser.java
+++ common/src/main/java/fi/fabianadrian/discordwhitelist/common/command/parser/MinecraftProfileParser.java
@@ -2,10 +2,10 @@ package fi.fabianadrian.discordwhitelist.common.command.parser;
 
 import fi.fabianadrian.discordwhitelist.common.command.ContextKeys;
 import fi.fabianadrian.discordwhitelist.common.profile.MinecraftProfile;
+import fi.fabianadrian.discordwhitelist.common.profile.resolver.ProfileResolver;
 import org.checkerframework.checker.nullness.qual.NonNull;
 import org.incendo.cloud.caption.Caption;
 import org.incendo.cloud.caption.CaptionVariable;
-import org.incendo.cloud.component.CommandComponent;
 import org.incendo.cloud.context.CommandContext;
 import org.incendo.cloud.context.CommandInput;
 import org.incendo.cloud.exception.parsing.ParserException;
@@ -13,34 +13,41 @@ import org.incendo.cloud.parser.ArgumentParseResult;
 import org.incendo.cloud.parser.ArgumentParser;
 import org.incendo.cloud.parser.ParserDescriptor;
 
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
 import java.util.regex.Pattern;
 
 
-public final class MinecraftProfileParser<C> implements ArgumentParser<C, MinecraftProfile> {
+public final class MinecraftProfileParser<C> implements ArgumentParser.FutureArgumentParser<C, MinecraftProfile> {
 	private static final Pattern PATTERN = Pattern.compile("^\\w{1,16}$");
 
 	public static <C> @NonNull ParserDescriptor<C, MinecraftProfile> minecraftProfileParser() {
 		return ParserDescriptor.of(new MinecraftProfileParser<>(), MinecraftProfile.class);
 	}
 
-	public static <C> CommandComponent.@NonNull Builder<?, MinecraftProfile> minecraftProfileComponent() {
-		return CommandComponent.<C, MinecraftProfile>builder().parser(minecraftProfileParser());
-	}
-
 	@Override
-	public @NonNull ArgumentParseResult<@NonNull MinecraftProfile> parse(@NonNull CommandContext<@NonNull C> context, @NonNull CommandInput commandInput) {
-		final String username = commandInput.peekString();
+	public @NonNull CompletableFuture<@NonNull ArgumentParseResult<MinecraftProfile>> parseFuture(@NonNull CommandContext<C> context, @NonNull CommandInput input) {
+		final String inputString = input.peekString();
+		final ProfileResolver resolver = context.get(ContextKeys.PROFILE_RESOLVER);
 
-		if (!PATTERN.matcher(username).matches()) {
-			return ArgumentParseResult.failure(new InvalidProfileNameException(username, context));
+		CompletableFuture<MinecraftProfile> future;
+		try {
+			UUID uuid = UUID.fromString(inputString);
+			future = resolver.resolve(uuid);
+		} catch (IllegalArgumentException exception) {
+			if (!PATTERN.matcher(inputString).matches()) {
+				return CompletableFuture.completedFuture(ArgumentParseResult.failure(new InvalidProfileNameException(inputString, context)));
+			}
+			future = resolver.resolve(inputString);
 		}
 
-		MinecraftProfile profile = context.get(ContextKeys.PROFILE_RESOLVER).resolve(username).join();
-		if (profile == null) {
-			return ArgumentParseResult.failure(new PlayerProfileParseException(username, context));
-		}
-		commandInput.readString();
-		return ArgumentParseResult.success(profile);
+		return future.thenApply(profile -> {
+			if (profile == null) {
+				return ArgumentParseResult.failure(new PlayerProfileParseException(inputString, context));
+			}
+			input.readString();
+			return ArgumentParseResult.success(profile);
+		});
 	}
 
 	public static final class InvalidProfileNameException extends ParserException {
