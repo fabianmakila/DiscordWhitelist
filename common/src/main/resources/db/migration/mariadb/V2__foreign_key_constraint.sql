deea9ef1359895f5c99ff8d98c03e7c580f5791e
Fabian Mäkilä <contact@fabianadrian.fi>
2026-02-26 16:08:12 +0200

    lets not cascade just yet

--- common/src/main/resources/db/migration/mariadb/V2__foreign_key_constraint.sql
+++ common/src/main/resources/db/migration/mariadb/V2__foreign_key_constraint.sql
@@ -5,4 +5,4 @@ ALTER TABLE dw_minecraft_profiles
     ADD CONSTRAINT fk_dw_minecraft_profiles_dw_discord_profiles_discord_identifier
         FOREIGN KEY (discord_identifier)
             REFERENCES dw_discord_profiles(discord_identifier)
-            ON DELETE CASCADE;
+            ON DELETE SET NULL;
